17ce289a4e93e83a20493ce8593e71a8a2fa8813
tamarha24-meet <139680727+tamarha24-meet@users.noreply.github.com>
2023-07-20 16:23:36 +0300

    minimum requirments

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,25 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'GET':
+		return render_template('home.html')
+
+	else:
+
+		author = request.form['author'].lower()
+		quote = request.form['quote']
+
+		login_session['author'] = author
+		login_session['quote'] = quote
+		try:
+			age = int(request.form['age'])
+			login_session['age'] = age
+			return render_template('thanks.html')
+		except:
+			return render_template('error.html')
+
 
 
 @app.route('/error')
@@ -18,8 +33,11 @@ def error():
 
 @app.route('/display')
 def display():
+	age = login_session['age']
+	quote = login_session['quote']
+	author = login_session['author']
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', age = age, author = author, quote = quote) # What variables are needed?
 
 
 @app.route('/thanks')
